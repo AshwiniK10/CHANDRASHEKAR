5c6880376204878e324b5365500108bc06c2912a
chandrashekarbr123456 <50397538+chandrashekarbr123456@users.noreply.github.com>
2019-05-08 15:34:11 +0530

    Update triangle_pattern.c

--- triangle_pattern.c
+++ triangle_pattern.c
@@ -3,7 +3,7 @@ int main()
 {
     int i, space, rows=5, k=0, count = 0, count1 = 0;
 
-    printf("Enter number of rows: ");
+    printf("Enter number of rows:\n ");
     //scanf("%d",&rows);
 
     for(i=1; i<=rows; ++i)
